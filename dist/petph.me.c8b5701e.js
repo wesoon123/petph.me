d07dee23a635bc152d5b9adfe1ae4d8376d299fe
unknown <wesson.cybertechcebu@gmail.com>
2025-10-24 15:46:41 +0800

    Deploy build

---
+++ dist/petph.me.c8b5701e.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){new Splide("#image-carousel",{heightRatio:.3,cover:!0,pagination:!0,breakpoints:{640:{heightRatio:.5}}}).mount()});
+//# sourceMappingURL=petph.me.c8b5701e.js.map
